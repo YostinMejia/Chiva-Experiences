127744c156463a68e749c2038e107723edbefc64
CodeNomad-01 <danielmoralestoro05@gmail.com>
2025-05-06 00:35:46 -0500

    apartado de categorias, info quemada

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// lib/main.dart
+
 import 'package:chiva_experiences/common/custom_bottom_nav_bar.dart';
 import 'package:chiva_experiences/common/top_bar.dart';
 import 'package:chiva_experiences/features/categorias/categoria_screen.dart';
@@ -13,11 +15,12 @@ void main() {
 class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // Usamos 'final' en lugar de 'const' y quitamos 'const' en cada pantalla
-    final pages = [
+    // Reordenamos para que coincida con:
+    // 0: Categoria, 1: Mapa, 2: Home, 3: Perfil
+    final pages = <Widget>[
       CategoriaScreen(),
-      HomeScreen(),
       MapaScreen(),
+      HomeScreen(),
       PerfilScreen(),
     ];
 
@@ -29,7 +32,6 @@ class MainApp extends StatelessWidget {
 
 class Home extends StatefulWidget {
   final List<Widget> pages;
-
   Home({required this.pages});
 
   @override
@@ -46,9 +48,7 @@ class _HomeState extends State<Home> {
       bottomNavigationBar: CustomBottomNavBar(
         currentIndex: _currentIndex,
         onTap: (index) {
-          setState(() {
-            _currentIndex = index;
-          });
+          setState(() => _currentIndex = index);
         },
       ),
       body: widget.pages[_currentIndex],
